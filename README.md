f2cd5915000b8a60e1577cee3ca64a1ff3936175
Daniel Taranger <nadenofficial@gmail.com>
2017-10-17 18:03:48 +0200

    weawee

---
+++ README.md
@@ -0,0 +1,9 @@
+Bash commands
+
+Get the code from GitHub
+    git clone https://github.com/DanielTaranger/modernjavascript.git
+Install dependencies
+    npm install express --save
+
+Start the web server.
+    node server.js
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+// app.get('/', (request, response) => response.send('Hello World'));
+
+app.get('/', function (request, response) {
+  response.send('Hello World');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`); // Note the `` backticks is NOT the same as single quotes ''
+});
